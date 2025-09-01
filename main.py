2528b62ab9aa352e695345b158c60315042420fc
Nuriel <kambatyrovnurdaulet@gmail.com>
2025-09-01 14:13:27 +0500

    add redis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import shutil
-
+import json
+import redis.asyncio as redis
 from fastapi import FastAPI, Request, Form, Depends, UploadFile, File
 from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -14,6 +15,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 templates = Jinja2Templates(directory="templates")
 
+# connect to Redis (default port is 6379)
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
 # Dependency for DB session
 def get_db():
     db = SessionLocal()
@@ -24,7 +27,16 @@ def get_db():
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request, db: Session = Depends(get_db)):
-    products = db.query(Product).all()
+    # check cache
+    cached_products = await redis_client.get("products")
+    if cached_products:
+        products = json.loads(cached_products)
+    else:
+        # query DB if not in cache
+        db_products = db.query(Product).all()
+        products = [{"id": p.id, "name": p.name, "price": p.price, "image":p.image} for p in db_products]
+        # save to cache for 60 sec
+        await redis_client.set("products", json.dumps(products), ex=60)
     return templates.TemplateResponse("index.html", {"request": request, "products": products})
 
 
