e4520ceb86fe4744874825e5fad42dac8bf4917d
Nuriel <kambatyrovnurdaulet@gmail.com>
2025-09-02 22:02:48 +0500

    fixed nginx

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/laminate_store"
+DATABASE_URL = "postgresql+psycopg2://postgres:1234@db:5432/laminate_store"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
